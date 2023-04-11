7fcf10721dd4aa14a400b235adbb23968550f499
SamuelAdlard <sammyadlard@gmail.com>
2023-04-11 20:55:42 +1000

    wroking

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     //first number in the multiplication problem
@@ -10,10 +11,30 @@ public class Enemy : MonoBehaviour
     public int number2;
     //Dislay text for the question
     public TextMeshPro text;
+    //Navmesh agent 
+    public NavMeshAgent agent;
 
+    //enemy freeze materials
+    public Material normalEnemy;
+    public Material frozenEnemy;
 
+    //player transform variable
+    Transform player;
+
+    //records the original speed
+    float moveSpeed;
     // Start is called before the first frame update
-    
+    private void Start()
+    {
+        //gets player object
+        player = GameObject.Find("Player").transform;
+
+        //records speed
+        moveSpeed = agent.speed;
+    }
+
+
+
     //called just after the enemy is instantiated and all the values have been set
     public void Spawn()
     {
@@ -24,6 +45,27 @@ public class Enemy : MonoBehaviour
     // Update is called once per frames
     void Update()
     {
-        
+        text.transform.rotation = Quaternion.Euler(90, 0, 0);
+        agent.SetDestination(player.position);
     }
+
+    //freezes the enemy
+    public IEnumerator Freeze()
+    {
+        
+        
+        print(moveSpeed);
+        //stops the agent from moving
+        agent.speed = 0;
+        //sets the frozen material
+        gameObject.GetComponent<Renderer>().material = frozenEnemy;
+        //waits for 2 seconds
+        yield return new WaitForSeconds(2);
+        //resets speed
+        agent.speed = moveSpeed;
+        //resets the material
+        gameObject.GetComponent<Renderer>().material = normalEnemy;
+    }
+
+
 }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 using TMPro;
 public class Manager : MonoBehaviour
 {
+    //player object
+    public GameObject player;
     
     //reference to the player's power
     public int playerPower = 10;
 
     //player display text
-    public TextMeshPro playerText; 
+    public TextMeshPro playerText;
+
+    //player materials
+    public Material normalMaterial;
+    public Material gracePeriodMaterial;
 
     //player's health
     public int health = 3;
@@ -29,6 +35,9 @@ public class Manager : MonoBehaviour
     //List of enemies
     public List<Enemy> enemies = new List<Enemy>();
 
+    //grace period
+    public bool gracePeriod = false;
+
     private void Start()
     {
         StartGame();
@@ -46,7 +55,7 @@ public class Manager : MonoBehaviour
         for (int i = 0; i < (4 + Mathf.Pow(currentWave, 2)); i++)
         {
             //Creates new enemy
-            Enemy newEnemy = Instantiate(enemyPrefab, new Vector3(Random.Range(-5, 5), 0.5f, Random.Range(-5, 5)), Quaternion.identity).GetComponent<Enemy>();
+            Enemy newEnemy = Instantiate(enemyPrefab, new Vector3(20, 0.5f, Random.Range(-10, 10)), Quaternion.identity).GetComponent<Enemy>();
             
             
             //Gets random numbers for the question
@@ -77,7 +86,7 @@ public class Manager : MonoBehaviour
     public void PlayerCollisionWithEnemy(Enemy enemy)
     {
         //Checks if the player's power is equal to the enemy's
-        if(playerPower == enemy.number1 * enemy.number2)
+        if(playerPower == enemy.number1 * enemy.number2 && !gracePeriod)
         {
             //Removes the enemy from the list
             enemies.Remove(enemy);
@@ -88,17 +97,21 @@ public class Manager : MonoBehaviour
             //makes sure there are still enemies left
             if (enemies.Count > 0)
             {
+                StartGracePeriod();
                 playerPower = GetPlayerPower();
             }
 
         }
-        else
+        else if(!gracePeriod)
         {
             //records the mistake
             mistakes++;
 
             //takes health away
             health--;
+
+            //freezes the enemy
+            enemy.StartCoroutine(enemy.Freeze());
         }
     }
 
@@ -108,10 +121,52 @@ public class Manager : MonoBehaviour
         int randomEnemy = Random.Range(0, enemies.Count);
         int newPower = enemies[randomEnemy].number1 * enemies[randomEnemy].number2;
         //Sets the display text to the correct number
-        playerText.text = (newPower).ToString();
+        playerText.text = newPower.ToString();
         //selects a new player power
         return enemies[randomEnemy].number1 * enemies[randomEnemy].number2;
     }
+
+    //Starts the grace period that allows the player to move away from the enemies
+    private void StartGracePeriod()
+    {
+        gracePeriod = true;
+
+        //gets the player movement script
+        PlayerMovement movementScript = player.GetComponent<PlayerMovement>();
+        //gets the player collider
+        Collider collider = player.GetComponent<Collider>();
+        //gets player speed
+        float speed = movementScript.Speed;
+        //sets new player speed
+        movementScript.Speed = speed + 1;
+        //turns off collider
+        collider.enabled = false;
+        //sets new material
+        player.GetComponent<MeshRenderer>().material = gracePeriodMaterial;
+        //starts timer to end the grace period
+        Invoke("EndGracePeriod", 2.5f);
+    }
+
+    //ends the grave period
+    private void EndGracePeriod()
+    {
+        gracePeriod = false;
+        
+        //gets player movement
+        PlayerMovement movementScript = player.GetComponent<PlayerMovement>();
+        //gets collider
+        Collider collider = player.GetComponent<Collider>();
+        //gets speed
+        float speed = movementScript.Speed;
+        //sets new speed
+        movementScript.Speed = speed - 1;
+        //turns collider on
+        collider.enabled = true;
+        //sets material
+        player.GetComponent<MeshRenderer>().material = normalMaterial;
+    }
+
+   
 
 }
 
