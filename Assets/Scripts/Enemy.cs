3d5db92ba00ef19ab07f7013688526e5d0f0bfb5
Samuel Adlard <72796569+SamuelAdlard@users.noreply.github.com>
2023-05-12 15:05:09 +1000

    animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,22 +20,37 @@ public class Enemy : MonoBehaviour
     public Material normalEnemy;
     public Material frozenEnemy;
 
+    //references the manager script
+    public Manager manager;
+
     //player transform variable
     Transform player;
 
     //records the original speed
     float moveSpeed;
 
+    //animation component
+    public Animator animator;
+
+    //records the state of the enemy to see if it is walking or not
+    bool walking = true;
+
     
 
     // Start is called before the first frame update
     private void Start()
     {
+        //Gets the manager script
+        manager = GameObject.Find("Manager").GetComponent<Manager>();
+        
         //gets player object
         player = GameObject.Find("Player").transform;
 
         //records speed
         moveSpeed = agent.speed;
+
+        //Gives each enemy a random animation speed
+        animator.speed = Random.Range(0.5f, 1);
     }
 
 
@@ -53,14 +68,31 @@ public class Enemy : MonoBehaviour
     // Update is called once per frames
     void Update()
     {
+        //sets the rotation to always align with the camera
         text.transform.rotation = Quaternion.Euler(90, 0, 0);
+        //plays the character animations
+        if (!manager.alive)
+        {
+            animator.SetBool("Celebrate", true);
+        }
+        else if(walking)
+        {
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Idle", true);
+        }
+        
+        //Makes the enemy walk towards the player
         agent.SetDestination(player.position);
     }
 
     //freezes the enemy
     public IEnumerator Freeze()
     {
-        
+        //Sets the enemy state to not walking
+        walking = false;
         
         print(moveSpeed);
         //stops the agent from moving
@@ -77,6 +109,9 @@ public class Enemy : MonoBehaviour
         gameObject.GetComponent<Renderer>().material = normalEnemy;
         //Turns on collider
         gameObject.GetComponent<Collider>().enabled = true;
+
+        //Sets the enemy state to walking
+        walking = true;
     }
 
     public void OnDestroy()
